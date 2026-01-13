261bfa0db4ffddf94629dcc00cbfa43ed833404e
devnadifa <devnadifa@gmail.com>
2026-01-13 20:24:57 +0300

    multiplication of floating numbers

---
+++ FloatMultiplication.java
@@ -0,0 +1,10 @@
+public class FloatMultiplication {
+  public static void main(String[] args) {
+    float num1 = 12.344f; //f is added to tell java it's a float number since doube numbers are the default
+    float num2 = 4997.9f;
+
+    float result = num1 * num2;
+    System.out.println(result);
+
+  }
+}
