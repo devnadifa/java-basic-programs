bb79460a08f23d80a9d872ef78c240ede7f5dcd5
devnadifa <devnadifa@gmail.com>
2026-01-15 07:53:57 +0300

    check whether number is odd or even

---
+++ EvenorOdd.java
@@ -0,0 +1,12 @@
+public class EvenorOdd {
+  public static void main(String[] args) {
+    int number = 4;
+    //check whether it's old or even
+    if (number % 2 == 0) {
+      System.out.println("The number " + number + " is an even number");
+    } else {
+      System.out.println("The number " + number + " is an odd  number");
+    }
+  }
+  
+}
