983732ac95d7242d8315af557f4bce9093af50c8
devnadifa <devnadifa@gmail.com>
2026-01-15 08:03:38 +0300

    finding largest number

---
+++ LargeNumber.java
@@ -0,0 +1,17 @@
+public class LargeNumber {
+  public static void main(String[] args) {
+    int a = 7;
+    int b = 8;
+    int c = 5;
+    
+    if (a > b) {
+      System.out.println("The largest number is " + a );
+    } else if(b > c) {
+      System.out.println("The largest number is " + b);
+    } else {
+      System.out.println("The largest number is " + c);
+    }
+
+  }
+  
+}
